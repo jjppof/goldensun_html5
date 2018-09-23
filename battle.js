dabf44faf49b90ee9711c2f756ae02f61314c84a
jjppof <jjpp.of@hotmail.com>
2018-09-23 11:50:00 -0300

    code refac

--- battle.js
+++ battle.js
@@ -30,7 +30,7 @@ var pos_x_enemy;
 var pos_y_enemy;
 var scale;
 
-var game = new Phaser.Game(
+var game = new Phaser.Game (
     GAME_WIDTH,
     GAME_HEIGHT,
     Phaser.AUTO,
@@ -58,9 +58,7 @@ function preload() {
 function create() {
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.input.onTap.add(function(pointer, isDoubleClick) {  
-        if(isDoubleClick) {    
-            game.scale.startFullScreen(true);
-        }  
+        if (isDoubleClick) game.scale.startFullScreen(true);
     });
 
     battle_bg = game.add.tileSprite(0, 17, GAME_WIDTH, 113, 'colosso');
@@ -87,12 +85,12 @@ function create() {
     group_enemy = game.add.group();
     group_party = game.add.group();
 
-    for(var i = 0; i < players_number; i++){
-        if(i < party_count){
-            var p = group_party.create(0, 0, 'felix_back');
-        } else{
-            var p = group_enemy.create(0, 0, 'mino_back');
-        }
+    for (let i = 0; i < players_number; i++) {
+        let p;
+        if (i < party_count)
+            p = group_party.create(0, 0, 'felix_back');
+        else
+            p = group_enemy.create(0, 0, 'mino_back');
         p.anchor.setTo(0.5, 1);
         p.scale.setTo(default_scale, default_scale);
         players.push(p);
@@ -103,31 +101,31 @@ function create() {
     first_enemy_char = group_enemy.children[0];
     last_enemy_char = group_enemy.children[party_count - 1];
 
-    game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(function(){
+    game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(() => {
         battle_bg.loadTexture('colosso');
         battle_bg2.loadTexture('colosso');
     }, this);
-    game.input.keyboard.addKey(Phaser.Keyboard.TWO).onDown.add(function(){
+    game.input.keyboard.addKey(Phaser.Keyboard.TWO).onDown.add(() => {
         battle_bg.loadTexture('kolima');
         battle_bg2.loadTexture('kolima');
     }, this);
-    game.input.keyboard.addKey(Phaser.Keyboard.THREE).onDown.add(function(){
+    game.input.keyboard.addKey(Phaser.Keyboard.THREE).onDown.add(() => {
         battle_bg.loadTexture('mercury');
         battle_bg2.loadTexture('mercury');
     }, this);
-    game.input.keyboard.addKey(Phaser.Keyboard.FOUR).onDown.add(function(){
+    game.input.keyboard.addKey(Phaser.Keyboard.FOUR).onDown.add(() => {
         battle_bg.loadTexture('desert');
         battle_bg2.loadTexture('desert');
     }, this);
-    game.input.keyboard.addKey(Phaser.Keyboard.FIVE).onDown.add(function(){
+    game.input.keyboard.addKey(Phaser.Keyboard.FIVE).onDown.add(() => {
         battle_bg.loadTexture('tunnel');
         battle_bg2.loadTexture('tunnel');
     }, this);
-    game.input.keyboard.addKey(Phaser.Keyboard.SIX).onDown.add(function(){
+    game.input.keyboard.addKey(Phaser.Keyboard.SIX).onDown.add(() => {
         battle_bg.loadTexture('vault');
         battle_bg2.loadTexture('vault');
     }, this);
-    game.input.keyboard.addKey(Phaser.Keyboard.SEVEN).onDown.add(function(){
+    game.input.keyboard.addKey(Phaser.Keyboard.SEVEN).onDown.add(() => {
         battle_bg.loadTexture('venus');
         battle_bg2.loadTexture('venus');
     }, this);
@@ -137,29 +135,34 @@ function create() {
 
 
 //active spin effect
-function spin(angle, easing, duration){
-    if(!spining){
+function spin(angle, easing, duration) {
+    if (!spining) {
         spining = true;
-        game.add.tween(camera_angle).to( { rad: camera_angle.rad + angle }, duration, easing, true);
-        game.time.events.add(duration + 50, function(){
+        game.add.tween(camera_angle).to(
+            {rad: camera_angle.rad + angle},
+            duration,
+            easing,
+            true
+        );
+        game.time.events.add(duration + 50, () => {
             spining = false;
         }, this);
     }
 }
 
 function update() {
-    if (cursors.left.isDown || cursors.right.isDown || spining){
+    if (cursors.left.isDown || cursors.right.isDown || spining) {
 
         //angle change and bg x position change
-        if(!cursors.left.isDown && cursors.right.isDown && !spining){
+        if (!cursors.left.isDown && cursors.right.isDown && !spining) {
             camera_angle.rad -= camera_speed;
             battle_bg.x -= bg_speed
-        } else if(cursors.left.isDown && !cursors.right.isDown && !spining){
+        } else if (cursors.left.isDown && !cursors.right.isDown && !spining) {
             camera_angle.rad += camera_speed; 
             battle_bg.x += bg_speed
         }
 
-        if(!spining){ //let spin effect do its work freely
+        if (!spining) { //let spin effect do its work freely
             if(camera_angle.rad >= FULL_ROUND) camera_angle.rad -= FULL_ROUND;
             if(camera_angle.rad < 0) camera_angle.rad += FULL_ROUND;
         } else //tie bg x position with camera angle when spining
@@ -167,15 +170,15 @@ function update() {
         old_camera_angle = camera_angle.rad;
 
         //check bg x position surplus
-        if(battle_bg.x > GAME_WIDTH)
+        if (battle_bg.x > GAME_WIDTH)
             battle_bg.x -= Math.abs(Math.floor(battle_bg.x/GAME_WIDTH)) * GAME_WIDTH;
-        else if(battle_bg.x < -GAME_WIDTH)
+        else if (battle_bg.x < -GAME_WIDTH)
             battle_bg.x += Math.abs(Math.floor(battle_bg.x/GAME_WIDTH)) * GAME_WIDTH;
 
         //make mirrored bg follow default bg
-        if(battle_bg.x > 0 && battle_bg.x < GAME_WIDTH)
+        if (battle_bg.x > 0 && battle_bg.x < GAME_WIDTH)
             battle_bg2.x = battle_bg.x - GAME_WIDTH;
-        else if(battle_bg.x < 0 && battle_bg.x > -GAME_WIDTH)
+        else if (battle_bg.x < 0 && battle_bg.x > -GAME_WIDTH)
             battle_bg2.x = battle_bg.x + GAME_WIDTH;
 
         //get equidistant arc lenghts from camera angle
@@ -185,34 +188,34 @@ function update() {
         //calculate party and enemy base position
         pos_x_party = center_x + ellipse(party_angle)*Math.cos(party_angle);
         pos_y_party = center_y + ellipse(party_angle)*Math.sin(party_angle);
-        pos_x_enemy  = center_x + ellipse(enemy_angle)*Math.cos(enemy_angle);
-        pos_y_enemy  = center_y + ellipse(enemy_angle)*Math.sin(enemy_angle);
+        pos_x_enemy = center_x + ellipse(enemy_angle)*Math.cos(enemy_angle);
+        pos_y_enemy = center_y + ellipse(enemy_angle)*Math.sin(enemy_angle);
 
         //check party and enemy z index
-        if(Math.sin(camera_angle.rad) > 0 && game.world.getChildIndex(group_party) < game.world.getChildIndex(group_enemy))
+        if (Math.sin(camera_angle.rad) > 0 && game.world.getChildIndex(group_party) < game.world.getChildIndex(group_enemy))
             game.world.swapChildren(group_enemy, group_party);
-        else if(Math.sin(camera_angle.rad) < 0 && game.world.getChildIndex(group_party) > game.world.getChildIndex(group_enemy))
+        else if (Math.sin(camera_angle.rad) < 0 && game.world.getChildIndex(group_party) > game.world.getChildIndex(group_enemy))
             game.world.swapChildren(group_enemy, group_party);
 
         //check party z index order
-        if(Math.cos(camera_angle.rad) < 0 && first_party_char.z > last_party_char.z)
+        if (Math.cos(camera_angle.rad) < 0 && first_party_char.z > last_party_char.z)
             group_party.reverse();
-        else if(Math.cos(camera_angle.rad) > 0 && first_party_char.z < last_party_char.z)
+        else if (Math.cos(camera_angle.rad) > 0 && first_party_char.z < last_party_char.z)
             group_party.reverse();
 
         //check enemy z index order
-        if(Math.cos(camera_angle.rad) < 0 && first_enemy_char.z < last_enemy_char.z)
+        if (Math.cos(camera_angle.rad) < 0 && first_enemy_char.z < last_enemy_char.z)
             group_enemy.reverse();
-        else if(Math.cos(camera_angle.rad) > 0 && first_enemy_char.z > last_enemy_char.z)
+        else if (Math.cos(camera_angle.rad) > 0 && first_enemy_char.z > last_enemy_char.z)
             group_enemy.reverse();
 
-        for(var i = 0; i < players_number; i++){
+        for (let i = 0; i < players_number; i++) {
             relative_angle = i < party_count ? camera_angle.rad : camera_angle.rad + Math.PI;
-            if(i < party_count){ //shift party players from base point
-                players[i].x = pos_x_party + ((spacing_distance*i - middle_shift_party) + (spacing_distance >> 1))*Math.sin(relative_angle);
+            if (i < party_count) { //shift party players from base point
+                players[i].x = pos_x_party + ((spacing_distance*i - middle_shift_party) + (spacing_distance >> 1)) * Math.sin(relative_angle);
                 players[i].y = pos_y_party;
             } else {  //shift enemy players from base point
-                players[i].x = pos_x_enemy + ((spacing_distance*(i-party_count) - middle_shift_enemy) + (spacing_distance >> 1))*Math.sin(relative_angle);
+                players[i].x = pos_x_enemy + ((spacing_distance*(i-party_count) - middle_shift_enemy) + (spacing_distance >> 1)) * Math.sin(relative_angle);
                 players[i].y = pos_y_enemy;
             }
 
@@ -221,35 +224,35 @@ function update() {
             players[i].scale.setTo(scale, scale);
 
             //change texture in function of position
-            if(i < party_count){
-                if(Math.sin(relative_angle) > 0 && players[i].key != 'felix_back')
+            if (i < party_count) {
+                if (Math.sin(relative_angle) > 0 && players[i].key != 'felix_back')
                     players[i].loadTexture('felix_back');
-                else if(Math.sin(relative_angle) <= 0 && players[i].key != 'felix_front')
+                else if (Math.sin(relative_angle) <= 0 && players[i].key != 'felix_front')
                     players[i].loadTexture('felix_front');
-            } else{
-                if(Math.sin(relative_angle) > 0 && players[i].key != 'mino_back')
+            } else {
+                if (Math.sin(relative_angle) > 0 && players[i].key != 'mino_back')
                     players[i].loadTexture('mino_back');
-                else if(Math.sin(relative_angle) <= 0 && players[i].key != 'mino_front')
+                else if (Math.sin(relative_angle) <= 0 && players[i].key != 'mino_front')
                     players[i].loadTexture('mino_front');
             }
 
             //change side in function of position
-            if(Math.cos(relative_angle) > 0 && players[i].scale.x < 0)
+            if (Math.cos(relative_angle) > 0 && players[i].scale.x < 0)
                 players[i].scale.setTo(players[i].scale.x, players[i].scale.y);
-            else if(Math.cos(relative_angle) <= 0 && players[i].scale.x > 0)
+            else if (Math.cos(relative_angle) <= 0 && players[i].scale.x > 0)
                 players[i].scale.setTo(-players[i].scale.x, players[i].scale.y);
         }
     }
 }
 
-function ellipse(angle){ //ellipse formula
+function ellipse(angle) { //ellipse formula
     return a*b/Math.sqrt(Math.pow(b*Math.cos(angle), 2) + Math.pow(a*Math.sin(angle), 2));
 }
 
-function get_angle(angle){ //equidistant ellipse angle formula
+function get_angle(angle) { //equidistant ellipse angle formula
     return angle + Math.atan(( (b-a)*Math.tan(angle) )/( a + b*Math.pow(Math.tan(angle), 2) ));
 }
 
-function get_scale(angle){ //scale formula
+function get_scale(angle) { //scale formula
     return (Math.sin(angle)/6 + 0.8334) * default_scale;
 }
