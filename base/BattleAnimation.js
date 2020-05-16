74b1d75a6d23fed690f02ee34341569cda8337e4
Joao Paulo Oliveira Fernandes <jjpp.of@gmail.com>
2020-05-16 00:52:16 -0300

    updating comments

--- base/BattleAnimation.js
+++ base/BattleAnimation.js
@@ -1,19 +1,20 @@
 export class BattleAnimation {
     //tween type can be 'Initial' for first position
     //tween type can be 'NoTween' for immediately change
-    //sprite_index: -2 is the target, -1 is the caster, 0... is the sprites_key_names index
+    //sprite_index: "target" is the target, "caster" is the caster, 0... is the sprites_key_names index
+    //values can be "target", "caster" or an actual value
     constructor(
         key_name,
-        sprites_key_names,
-        x_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value}
-        y_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value}
-        x_scale_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value}
-        y_scale_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value}
-        alpha_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value}
-        rotation_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value, direction: value}
-        stage_angle_sequence, //{start_at: value, to: value, tween: type, duration: value, direction: value}
-        hue_angle_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value, direction: value}
-        tint_sequence, //{start_at: value, sprite_index: index, to: value, tween: type, duration: value}
+        sprites, //{key_name: string, per_target: bool}
+        x_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value}
+        y_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value}
+        x_scale_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value}
+        y_scale_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value}
+        alpha_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value}
+        rotation_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value, direction: value}
+        stage_angle_sequence, //{start_at: value, to: value, is_absolute: bool, tween: type, duration: value, direction: value}
+        hue_angle_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value, direction: value}
+        tint_sequence, //{start_at: value, sprite_index: index, to: value, is_absolute: bool, tween: type, duration: value}
         camera_shake_sequence, //{start_at: value, intensity: value, duration: value: direction: direction_option}
         play_sequence, //{start_at: value, sprite_index: index, reverse: bool, frame_rate: value, repeat: bool}
         set_frame_sequence, //{start_at: value, frame_key_name: string}
@@ -21,7 +22,7 @@ export class BattleAnimation {
         is_party_animation
     ) {
         this.key_name = key_name;
-        this.sprites_key_names = sprites_key_names;
+        this.sprites = sprites;
         this.x_sequence = x_sequence;
         this.y_sequence = y_sequence;
         this.x_scale_sequence = x_scale_sequence;
