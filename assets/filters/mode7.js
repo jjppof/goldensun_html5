db89051550d1d121771108a7b107553b4d67d15c
Joao Paulo Oliveira Fernandes <jjpp.of@hotmail.com>
2020-10-19 22:24:04 -0300

    setting some mode7 values

--- assets/filters/mode7.js
+++ assets/filters/mode7.js
@@ -2,13 +2,13 @@ Phaser.Filter.Mode7 = function (game) {
     Phaser.Filter.call(this, game);
 
     this.uniforms.angle = {type: "1f", value: 0};
-    this.uniforms.sin1 = {type: "1f", value: 0};
+    this.uniforms.sin1 = {type: "1f", value: 1};
     this.uniforms.sin2 = {type: "1f", value: 0};
     this.uniforms.cos1 = {type: "1f", value: 0};
-    this.uniforms.cos2 = {type: "1f", value: 0};
+    this.uniforms.cos2 = {type: "1f", value: -1};
     this.uniforms.scale = {type: "1f", value: .28};
     this.uniforms.distance = {type: "1f", value: 1};
-    this.uniforms.lookY = {type: "1f", value: 4};
+    this.uniforms.lookY = {type: "1f", value: 3.5};
 
     this.fragmentSrc = [
         "precision mediump float;",
@@ -31,33 +31,20 @@ Phaser.Filter.Mode7 = function (game) {
         "    vec2 uv = vTextureCoord;",
         "    vec2 warped;",
 
-        "    // perform mode7 transform on uvs",
-        "    warped = vec2(uv.x-0.5, 4)",
-        "           / vec2(uv.y+lookY-0.5, uv.y+lookY-0.5);",
-        "    warped.y -= distance-0.04;",
+        // perform mode7 transform on uvs
+        "    warped = vec2(uv.x-0.5, 4) / vec2(uv.y + lookY, uv.y + lookY);",
+        "    warped.y -= distance;",
         "    warped /= scale;",
 
-        "    // rotate the new uvs",
-        "    //warped *= mat2(sin1, sin2, cos1, cos2);",
-        "    warped *= mat2(sin1, sin2, cos1, cos2);",
-        "    // centred",
-        "    warped += vec2(0.5, 0.5);",
-        "    /* set the uvs to repeat in space",
-        "    warped = vec2(mod(warped.x, 1.0), mod(warped.y, 1.0)); // */",
-        "    ",
+        "    warped *= mat2(sin1, sin2, cos1, cos2);", // rotate the new uvs
+        "    warped += vec2(0.5, 0.5);", // centred
+
         "    bool isDraw = uv.y > 0.5 - lookY;",
-        "    //isDraw = warped.x >= 0.0 && warped.y >= 0.0  && warped.x <= 1.0 &&warped.y<=1.0;",
-        "    ",
         "    if (isDraw){",
-        "       gl_FragColor = texture2D(uSampler, warped);",
+        "        gl_FragColor = texture2D(uSampler, warped);",
         "    }else{",
         "        gl_FragColor = vec4(0.0,0.0,0.0,0.0);",
         "    }",
-        "    /*uv = uv*mat2(",
-        "        1.0, 0.0,",
-        "        0.0, 1.0",
-        "    );*/",
-        "    //gl_FragColor = texture2D(uSampler, uv);",
         "}",
     ];
 };
