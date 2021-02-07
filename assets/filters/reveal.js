c1a10456bcfae82b59545c8990a3c7afee6cc1f7
Joao Paulo Oliveira Fernandes <jjpp.of@hotmail.com>
2021-02-07 17:57:07 -0300

    being able to control height and width

--- assets/filters/reveal.js
+++ assets/filters/reveal.js
@@ -2,7 +2,9 @@ Phaser.Filter.Reveal = function (game) {
     Phaser.Filter.call(this, game);
 
     this.uniforms.phase = {type: "1f", value: 0};
-    this.uniforms.frequency = {type: "1f", value: 66};
+    this.uniforms.freq = {type: "1f", value: 66};
+    this.uniforms.a = {type: "1f", value: 0.2};
+    this.uniforms.b = {type: "1f", value: 0.16};
 
     this.fragmentSrc = [
         "precision mediump float;",
@@ -14,18 +16,19 @@ Phaser.Filter.Reveal = function (game) {
         "precision highp float;",
 
         "uniform highp float phase;",
-        "uniform highp float frequency;",
+        "uniform highp float freq;",
+        "uniform highp float a;",
+        "uniform highp float b;",
 
         "void main(void) {",
             "float y = vTextureCoord.y;",
             //Reveal formula:
-            //gain * sin(freq * y + phase) * (y^4 - 1) + (y - 0.5)^2/a + (x = 0.5)^2/b = 1
+            //gain * sin(freq * y + phase) * (16*(y - 0.5)^4 - 1) + (y - 0.5)^2/a + (x - 0.5)^2/b - 1 = 0
             //gain = 0.0684, freq = 66, phase = 0, a = 0.2, b = 0.16
-            "float aux = -171.*(pow(y, 4.) - 1.)*sin(phase+frequency*y)-625.*(20.*y*y -20.*y+1.);",
-            "float x = aux < 0. ? -1. : .5 + 0.008*(vTextureCoord.x >= .5 ? sqrt(aux) : -sqrt(aux));",
+            "float aux = -a*b*(2736.*a*pow(y,4.)*sin(phase+freq*y)-5472.*a*pow(y,3.)*sin(phase+freq*y)+4104.*a*y*y*sin(phase+freq*y)-1368.*a*y*sin(phase+freq*y)-2500.*a+2500.*y*y-2500.*y+625.);",
+            "float x = aux < 0. ? -1. : (25.*a + (vTextureCoord.x >= .5 ? sqrt(aux) : -sqrt(aux)))/(50.*a);",
             "float sqr_rho = x < 0. ? -1. : pow(x - .5, 2.) + pow(y - .5, 2.);",
             "float sqr_orig_rho = pow(vTextureCoord.x - .5, 2.) + pow(vTextureCoord.y - .5, 2.);",
-            "gl_FragColor = texture2D(uSampler, vTextureCoord);",
             "gl_FragColor.rgba = vec4(0,0,0,sqr_orig_rho<sqr_rho ? 0 : 1);",
         "}",
     ];
@@ -43,11 +46,29 @@ Object.defineProperty(Phaser.Filter.Reveal.prototype, 'phase', {
     }
 });
 
-Object.defineProperty(Phaser.Filter.Reveal.prototype, 'frequency', {
+Object.defineProperty(Phaser.Filter.Reveal.prototype, 'freq', {
     get: function() {
-        return this.uniforms.frequency.value;
+        return this.uniforms.freq.value;
     },
     set: function(value) {
-        this.uniforms.frequency.value = value;
+        this.uniforms.freq.value = value;
+    }
+});
+
+Object.defineProperty(Phaser.Filter.Reveal.prototype, 'a', {
+    get: function() {
+        return this.uniforms.a.value;
+    },
+    set: function(value) {
+        this.uniforms.a.value = value;
+    }
+});
+
+Object.defineProperty(Phaser.Filter.Reveal.prototype, 'b', {
+    get: function() {
+        return this.uniforms.b.value;
+    },
+    set: function(value) {
+        this.uniforms.b.value = value;
     }
 });
