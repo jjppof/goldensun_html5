9951390c984c6e7bd1305b9e07dd8393dd498ea8
Joao Paulo Oliveira Fernandes <jjpp.of@hotmail.com>
2021-04-21 23:13:10 -0300

    adding some docs to utils

--- base/utils.ts
+++ base/utils.ts
@@ -1,11 +1,12 @@
 import * as numbers from "./magic_numbers";
 import * as _ from "lodash";
 
-/*Font names*/
+/** Default font name */
 export const FONT_NAME = "gs-bmp-font";
+/** Italic font name */
 export const ITALIC_FONT_NAME = "gs-italic-bmp-font";
 
-/*Element keys*/
+/** Element keys */
 export enum elements {
     VENUS = "venus",
     MERCURY = "mercury",
@@ -15,10 +16,10 @@ export enum elements {
     ALL_ELEMENTS = "all_elements",
 }
 
-/*Default elements order*/
+/** Default elements order */
 export const ordered_elements = [elements.VENUS, elements.MERCURY, elements.MARS, elements.JUPITER];
 
-/*Element names*/
+/** Element names */
 export const element_names = {
     [elements.VENUS]: "Earth",
     [elements.MERCURY]: "Water",
@@ -27,7 +28,7 @@ export const element_names = {
     [elements.ALL_ELEMENTS]: "All elements",
 };
 
-/*Element colors*/
+/** Element colors */
 export const element_colors = {
     [elements.VENUS]: 0xfef116,
     [elements.MERCURY]: 0x0ad2ef,
@@ -35,7 +36,7 @@ export const element_colors = {
     [elements.JUPITER]: 0xe070b0,
 };
 
-/*Element colors in battle*/
+/** Element colors in battle */
 export const element_colors_in_battle = {
     [elements.VENUS]: 0xf8f848,
     [elements.MERCURY]: 0x80f8f8,
@@ -44,7 +45,7 @@ export const element_colors_in_battle = {
     [elements.NO_ELEMENT]: 0xc6c6c6,
 };
 
-/*8-Directional direction values*/
+/** 8-Directional direction values */
 export enum directions {
     right = 0,
     down_right = 1,
@@ -56,7 +57,7 @@ export enum directions {
     up_right = 7,
 }
 
-/*8-Directional direction keys*/
+/** 8-Directional direction keys */
 export const reverse_directions = {
     [directions.right]: "right",
     [directions.up_right]: "up_right",
@@ -68,6 +69,7 @@ export const reverse_directions = {
     [directions.down_right]: "down_right",
 };
 
+/** Base actions of a controllable char */
 export enum base_actions {
     IDLE = "idle",
     WALK = "walk",
@@ -80,17 +82,25 @@ export enum base_actions {
     GRANT = "grant",
 }
 
+/**
+ * Generate a mask for the given direction.
+ * Examples:
+ *  - The given direction is left, which is 4, then this function will return: 00010000
+ *  - The given direction is right, which is 0, then this function will return: 00000001
+ * @param direction The direction to generate the mask.
+ * @returns Returns the mask.
+ */
 export function get_direction_mask(direction: directions) {
     if (direction === null) return 0;
     return direction === 0 ? 1 : 2 << (direction - 1);
 }
 
-/*Returns the direction values for diagonal directions
-Example: Input: 7 (up_right) / Output: [6,0]
-
-Input: direction [number] - Diagonal direction value
-
-Output: [array] - Array with split direction values*/
+/**
+ * Returns the direction values for diagonal directions
+ * Example: Input: 7 (up_right) / Output: [6,0]
+ * @param direction Diagonal direction value
+ * @returns Array with split direction values
+ */
 export function split_direction(direction: directions) {
     if (direction % 2 === 0) return [direction];
     const vals: directions[] = new Array(2);
@@ -107,72 +117,83 @@ export function split_direction(direction: directions) {
  * @param {number} dir_2 - Direction values
  * @return {number} Diagonal direction value
  */
-export function join_directions(dir_1, dir_2) {
+export function join_directions(dir_1: directions, dir_2: directions): directions {
     dir_2 = dir_1 === directions.up && dir_2 === directions.right ? 8 : dir_2;
     return Math.min(dir_1, dir_2) + 1;
 }
 
-export function direction_range(direction: directions) {
+/**
+ * Given a direction, returns this direction and its adjacent directions.
+ * @param direction the direction value.
+ * @returns Returns the range of the given direction.
+ */
+export function direction_range(direction: directions): directions[] {
     return [(direction + 7) % 8, direction, (direction + 1) % 8];
 }
 
-/*Returns a random number (0,4)
-
-Output: [number] - Random number from 0 to 4*/
+/**
+ * Returns a random number from 0 to 4.
+ * @returns  Random number from 0 to 4
+ */
 export function variation() {
     return _.random(0, 4);
 }
 
-/*Places the angle (radians) in the [0,2*PI] range
-
-Input: angle [number] - Angle in radians
-Output: [number] - Angle in the [0,2*PI] range*/
+/**
+ * Places the angle (radians) in the [0,2*PI[ range.
+ * @param angle any angle in radians.
+ * @returns Angle in the [0,2*PI[ range.
+ */
 export function range_360(angle: number) {
     angle = angle % numbers.degree360;
     angle = angle < 0 ? angle + numbers.degree360 : angle;
     return angle;
 }
 
-/*Returns the opposite of the given direction
-
-Input: direction [number] - Direction value
-
-Output: [number] - Opposite direction value*/
-export function get_opposite_direction(direction: directions) {
+/**
+ * Returns the opposite of the given direction.
+ * @param direction Direction value
+ * @returns Opposite direction value
+ */
+export function get_opposite_direction(direction: directions): directions {
     return (direction + 4) % 8;
 }
 
-/*Apply the transition directions
-Used when being forced to face a different direction
-
-Input: current_direction [number] - Current direction value
-       desired_direction [number] - Desired direction value
-
-Output: [number] - The direction value to apply*/
-export function get_transition_directions(current_direction: directions, desired_direction: directions) {
+/**
+ * Gets the directions between the current direction and a desired/target direction.
+ * Example of transition between left and right: left -> up left -> up -> up right -> right.
+ * @param current_direction Current direction value
+ * @param desired_direction Desired/Target direction value
+ * @returns The direction value to apply
+ */
+export function get_transition_directions(current_direction: directions, desired_direction: directions): directions {
     const diff = desired_direction - current_direction;
     if (diff === 0) return current_direction;
     const sign = diff === 4 ? -1 : Math.sign(diff);
     return (current_direction + (Math.abs(diff) >= 4 ? -sign : sign) + 8) % 8;
 }
 
-/*Obtains the text width in pixels (INEFFICIENT)
-
-Input: game [Phaser:Game] - Reference to the running game object
-       text [string] - Text string*/
+/**
+ * Obtains the text width in pixels (INEFFICIENT).
+ * @param game the Phaser.Game instance
+ * @param text the text string to measure width
+ * @param italic whether the text is in italic
+ * @returns
+ */
 export function get_text_width(game, text, italic = false) {
     //get text width in px (dirty way)
     const font_name = italic ? ITALIC_FONT_NAME : FONT_NAME;
-    let text_sprite = game.add.bitmapText(0, 0, font_name, text, numbers.FONT_SIZE);
+    const text_sprite = game.add.bitmapText(0, 0, font_name, text, numbers.FONT_SIZE);
     const text_width = text_sprite.width;
     text_sprite.destroy();
     return text_width;
 }
 
-/*Either kills or destroys each sprite in the group
-
-Input: group [Phaser:Group] - The parent group
-       destroy [boolean] - If true, child is destroyed instead.*/
+/**
+ * Either kills or destroys each sprite in the group
+ * @param group The parent group
+ * @param destroy If true, child is destroyed instead
+ */
 export function kill_all_sprites(group, destroy = false) {
     group.children.forEach(child => {
         if (destroy) child.parent.remove(child, true);
@@ -180,13 +201,15 @@ export function kill_all_sprites(group, destroy = false) {
     });
 }
 
-/*Returns the surrounding positions
-Diagonals are optional
-
-Input: x,y [number] - The body's position
-       with_diagonals [boolean] - If true, includes diagonals
-       shift [number] - Distance to check*/
-export function get_surroundings(x, y, with_diagonals = false, shift = 1) {
+/**
+ * Returns the surrounding positions. Diagonals are optional.
+ * @param x the x reference position.
+ * @param y the y reference position.
+ * @param with_diagonals if true, includes diagonals.
+ * @param shift how distant if the surrounding positions from the given position.
+ * @returns Returns the surrounding positions.
+ */
+export function get_surroundings(x: number, y: number, with_diagonals = false, shift = 1) {
     let surroundings = [
         {x: x - shift, y: y, diag: false, direction: directions.left},
         {x: x + shift, y: y, diag: false, direction: directions.right},
@@ -204,9 +227,11 @@ export function get_surroundings(x, y, with_diagonals = false, shift = 1) {
     return surroundings;
 }
 
-/*Lists all directions, diagonals optional
-
-Input: with_diagonals [boolean] - If true, includes diagonals*/
+/**
+ * Lists all directions, diagonals optional.
+ * @param with_diagonals If true, includes diagonals.
+ * @returns Returns the directions.
+ */
 export function get_directions(with_diagonals = false) {
     const dirs = [directions.up, directions.down, directions.left, directions.right];
     if (with_diagonals) {
@@ -215,6 +240,11 @@ export function get_directions(with_diagonals = false) {
     return dirs;
 }
 
+/**
+ * Converts a color from hex to rgb.
+ * @param hex the color in hex format.
+ * @returns the color in rgb format.
+ */
 export function hex2rgb(hex: string | number) {
     if (typeof hex === "string") {
         hex = hex.replace(/^\s*#|\s*$/g, "");
@@ -232,13 +262,13 @@ export function hex2rgb(hex: string | number) {
     return {r: r, g: g, b: b};
 }
 
-/*Changes the brightness of a given color code
-
-Input: hex [number] - Input color
-       percent [number] - Brightness factor
-
-Output [number] - Output color*/
-export function change_brightness(hex, percent) {
+/**
+ * Changes the brightness of a given color code.
+ * @param hex the input color in hex.
+ * @param percent how much the brightness should change.
+ * @returns returns the new color in hex.
+ */
+export function change_brightness(hex: string | number, percent: number) {
     let {r, g, b} = hex2rgb(hex);
     let h, s, v;
     [h, s, v] = rgb2hsv(r, g, b);
@@ -249,37 +279,40 @@ export function change_brightness(hex, percent) {
     return parseInt(hex, 16);
 }
 
-/*Transform RGB color into HSV color
-
-Input: r,g,b [number] - Red, Green, Blue channels
-
-Output [array] - Hue, Saturation, Value channels (array of number)*/
-export function rgb2hsv(r, g, b) {
-    let v = Math.max(r, g, b),
+/**
+ * Transforms RGB color into HSV color.
+ * @param r red channel color value [0, 255]
+ * @param g green channel color value [0, 255]
+ * @param b blue channel color value [0, 255]
+ * @returns returns the color in HSV
+ */
+export function rgb2hsv(r: number, g: number, b: number) {
+    const v = Math.max(r, g, b),
         n = v - Math.min(r, g, b);
-    let h = n && (v === r ? (g - b) / n : v === g ? 2 + (b - r) / n : 4 + (r - g) / n);
+    const h = n && (v === r ? (g - b) / n : v === g ? 2 + (b - r) / n : 4 + (r - g) / n);
     return [60 * (h < 0 ? h + 6 : h), v && n / v, v];
 }
 
-/*Transform HSV color into RGB color
-
-Input: h,s,v [number] - Hue, Saturation, Value channels
-
-Output [array] - Red, Green, Blue (array of number)*/
-export function hsv2rgb(h, s, v) {
+/**
+ * Transforms HSV color into RGB color
+ * @param h hue channel color value
+ * @param s saturation channel color value
+ * @param v value channel color value
+ * @returns returns the color in RGB
+ */
+export function hsv2rgb(h: number, s: number, v: number) {
     let f = (n, k = (n + h / 60) % 6) => v - v * s * Math.max(Math.min(k, 4 - k, 1), 0);
     return [f(5), f(3), f(1)];
 }
 
-/*Defines the collision polygon
-
-Input: width [number] - Width of the body
-       shift [number] - Shift value
-       bevel [number] - Body's bevel value
-
-Output: [array] - Multidimensional array with points*/
-export function mount_collision_polygon(width, shift, bevel) {
-    if (bevel === undefined) bevel = 0;
+/**
+ * Creates a collision polygon.
+ * @param width Width of the body
+ * @param shift Shift value
+ * @param bevel Body's bevel value
+ * @returns Returns a list of coodinates of the polygon.
+ */
+export function mount_collision_polygon(width: number, shift: number, bevel: number = 0) {
     return [
         [bevel + shift, shift],
         ...(bevel === 0 ? [] : [[width - bevel + shift, shift]]),
