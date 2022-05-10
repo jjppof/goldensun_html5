c58ebd574bd1ae9e6528d8dca607ab10bda17e14
Joao Paulo Oliveira Fernandes <jjpp.of@hotmail.com>
2022-05-10 18:23:12 -0300

    additional checks for destroyer event

--- base/game_events/DestroyerEvent.ts
+++ base/game_events/DestroyerEvent.ts
@@ -3,9 +3,6 @@ import {GameEvent, event_types} from "./GameEvent";
 export class DestroyerEvent extends GameEvent {
     private target_event_key: string;
 
-    //these were the basic things that every child class had, so I took it.
-    //though the destroyer is just like a trigger, press it and it kills an event.
-    //it only needs target_event_key imo.
     constructor(game, data, active, key_name, target_event_key) {
         super(game, data, event_types.DESTROYER, active, key_name);
         this.target_event_key = target_event_key;
@@ -13,10 +10,14 @@ export class DestroyerEvent extends GameEvent {
 
     _fire() {
         if (this.target_event_key !== undefined) {
-            let target_event = GameEvent.get_labeled_event(this.target_event_key);
-            target_event.destroy();
+            const target_event = GameEvent.get_labeled_event(this.target_event_key);
+            if (target_event) {
+                target_event.destroy();
+            } else {
+                console.warn(`Could not find an event with "${this.target_event_key}" key.`);
+            }
         } else {
-            console.warn("The Target event is undefined.");
+            console.warn('The target event is undefined. Check "target_event_key" property.');
         }
     }
 
