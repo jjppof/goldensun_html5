c40c016f6b3c1f38e78e29d2e1c0e1089f544b68
Joao Paulo Oliveira Fernandes <jjpp.of@hotmail.com>
2022-05-10 10:00:17 -0300

    change control to create in custom bodies event

--- base/game_events/CustomCollisionBodyEvent.ts
+++ base/game_events/CustomCollisionBodyEvent.ts
@@ -2,7 +2,7 @@ import {GameEvent, event_types} from "./GameEvent";
 
 export class CustomCollisionBodyEvent extends GameEvent {
     private label: string;
-    private control: "create" | "destroy";
+    private create: boolean;
     private x: number;
     private y: number;
     private body_type: "box" | "circle" | "polygon";
@@ -14,10 +14,10 @@ export class CustomCollisionBodyEvent extends GameEvent {
         collision_layer?: number;
     };
 
-    constructor(game, data, active, key_name, label, control, x, y, body_type, properties) {
+    constructor(game, data, active, key_name, label, create, x, y, body_type, properties) {
         super(game, data, event_types.CUSTOM_COLLISION_BODY, active, key_name);
         this.label = label;
-        this.control = control;
+        this.create = create;
         this.x = x;
         this.y = y;
         this.body_type = body_type;
@@ -25,9 +25,9 @@ export class CustomCollisionBodyEvent extends GameEvent {
     }
 
     async _fire() {
-        if (this.control === "create") {
+        if (this.create) {
             this.data.collision.create_custom_body(this.label, this.x, this.y, this.body_type, this.properties);
-        } else if (this.control === "destroy") {
+        } else {
             this.data.collision.destroy_custom_body(this.label);
         }
     }
