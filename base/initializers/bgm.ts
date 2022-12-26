a4ae40a9314829de9c169632f8b82324a7c0a939
Joao Paulo Oliveira Fernandes <jjpp.of@gmail.com>
2022-12-26 11:53:31 -0300

    refactor bgm db

--- base/initializers/bgm.ts
+++ base/initializers/bgm.ts
@@ -2,8 +2,7 @@ import {GoldenSun} from "../GoldenSun";
 
 export function initialize_bgm_data(game: Phaser.Game, data: GoldenSun, bgm_db: any, load_promise_resolve: () => void) {
     const promises = [];
-    for (let bgm_key in bgm_db) {
-        const bgm_data = bgm_db[bgm_key];
+    for (let bgm_data of bgm_db) {
         const loader = game.load.audio(bgm_data.key, [bgm_data.path]);
         let promise_resolve;
         promises.push(new Promise(resolve => (promise_resolve = resolve)));
