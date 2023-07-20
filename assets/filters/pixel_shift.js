c6ad529751a690846a06d2d84c3229ba8db55204
Joao Paulo Oliveira Fernandes <jjpp.of@gmail.com>
2023-07-19 23:23:41 -0300

    pixel shift fix

--- assets/filters/pixel_shift.js
+++ assets/filters/pixel_shift.js
@@ -5,8 +5,8 @@ Phaser.Filter.PixelShift = function (game) {
     this.uniforms.y_shift = { type: '1f', value: 0.0 };
     this.uniforms.frame_width = { type: '1f', value: 0.0 };
     this.uniforms.frame_height = { type: '1f', value: 0.0 };
-    this.uniforms.base_texture_width = { type: '1f', value: 0.0 };
-    this.uniforms.base_texture_height = { type: '1f', value: 0.0 };
+
+    this.setResolution(game.config.width, game.config.height);
 
     this.fragmentSrc = `
         precision mediump float;
@@ -17,23 +17,21 @@ Phaser.Filter.PixelShift = function (game) {
         uniform float      y_shift;
         uniform float      frame_width;
         uniform float      frame_height;
-        uniform float      base_texture_width;
-        uniform float      base_texture_height;
+        uniform vec2       resolution;
 
         void main(void) {
-            vec2 base_texture_size = vec2(base_texture_width, base_texture_height);
             vec2 uv = vTextureCoord.xy;
-            vec2 uv_px = uv * base_texture_size;
+            vec2 uv_px = uv * resolution;
             float mod_pos;
 
             if (uv_px.y >= 0.0 && uv_px.y <= frame_height) {
                 mod_pos = mod(uv_px.y + y_shift, frame_height);
-                uv.y = mod_pos / base_texture_size.y;
+                uv.y = mod_pos / resolution.y;
             }
 
-            if (uv_px.x >= 0.0 && uv_px.x <= frame_width * 2.0) {
-                mod_pos = mod(uv_px.x + x_shift, frame_width * 2.0);
-                uv.x = mod_pos / base_texture_size.x;
+            if (uv_px.x >= 0.0 && uv_px.x <= frame_width) {
+                mod_pos = mod(uv_px.x + x_shift, frame_width);
+                uv.x = mod_pos / resolution.x;
             }
 
             gl_FragColor = texture2D(uSampler, uv);
@@ -78,23 +76,5 @@ Object.defineProperty(Phaser.Filter.PixelShift.prototype, 'frame_height', {
     },
     set: function(value) {
         this.uniforms.frame_height.value = value;
-    }
-});
-
-Object.defineProperty(Phaser.Filter.PixelShift.prototype, 'base_texture_width', {
-    get: function() {
-        return this.uniforms.base_texture_width.value;
-    },
-    set: function(value) {
-        this.uniforms.base_texture_width.value = value;
-    }
-});
-
-Object.defineProperty(Phaser.Filter.PixelShift.prototype, 'base_texture_height', {
-    get: function() {
-        return this.uniforms.base_texture_height.value;
-    },
-    set: function(value) {
-        this.uniforms.base_texture_height.value = value;
     }
 });
